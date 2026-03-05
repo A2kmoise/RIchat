15782fbe292705244adc8b379b57ce83db014dc0
A2kmoise <abayomoise950@gmail.com>
2026-03-05 21:59:25 +0200

    -{ * * }- first models

---
+++ src/main/java/com/example/rib/Imodel/User.java
@@ -0,0 +1,4 @@
+package com.example.rib.Imodel;
+
+public class User {
+}
---
+++ src/test/java/com/example/rib/RibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
