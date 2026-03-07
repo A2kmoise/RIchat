b3ba4a482289d5f3d93293a3426aef7e44546029
A2kmoise <abayomoise950@gmail.com>
2026-03-07 12:05:44 +0200

    -{ * * }- conversationParticipants model

--- src/main/java/com/example/rib/Imodel/ConversationParticipants.java
+++ src/main/java/com/example/rib/Imodel/ConversationParticipants.java
@@ -1,4 +1,25 @@
 package com.example.rib.Imodel;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "conversationParticipants")
 public class ConversationParticipants {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+   @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "conversation_id", nullable = false)
+   private Conversation conversation;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @Column(name = "joinedAt", nullable = false)
+    @CreationTimestamp
+    private Instant joinedAt;
+
+
 }
