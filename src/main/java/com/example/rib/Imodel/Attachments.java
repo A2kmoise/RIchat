287cec8b33dd34e3633620b31e37e4db7ffd01d7
A2kmoise <abayomoise950@gmail.com>
2026-03-07 08:59:06 +0200

    -{ * * }- attach model

--- src/main/java/com/example/rib/Imodel/Attachments.java
+++ src/main/java/com/example/rib/Imodel/Attachments.java
@@ -1,4 +1,24 @@
 package com.example.rib.Imodel;
 
+import com.example.rib.Ienum.AttachmentType;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="attachments")
 public class Attachments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "attachmentType", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AttachmentType type;
+
+    @Column(name = "source", nullable = false)
+    private  String fileUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="conversation_id", nullable = false)
+    private Messages messageSource;
+
 }
