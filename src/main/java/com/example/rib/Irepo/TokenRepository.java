43bd9cf631405d39caf623b5bb5331af73969e32
A2kmoise <abayomoise950@gmail.com>
2026-03-08 08:28:37 +0200

    -{ * * }- Token Repository

---
+++ src/main/java/com/example/rib/Irepo/TokenRepository.java
@@ -0,0 +1,7 @@
+package com.example.rib.Irepo;
+
+import com.example.rib.Imodel.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, String> {
+}
