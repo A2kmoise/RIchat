a3fabd1dc2199650c226e645c2febba104c2f6bc
A2kmoise <abayomoise950@gmail.com>
2026-03-08 08:28:06 +0200

    -{ * * }- ConversationParticipantsRepository

---
+++ src/main/java/com/example/rib/Irepo/ConversationParticipantsRepository.java
@@ -0,0 +1,7 @@
+package com.example.rib.Irepo;
+
+import com.example.rib.Imodel.ConversationParticipants;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConversationParticipantsRepository extends JpaRepository<ConversationParticipants, String> {
+}
