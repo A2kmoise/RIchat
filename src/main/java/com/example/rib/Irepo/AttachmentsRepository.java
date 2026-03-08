21bfd5ba59d132064d31af29f7ab3ffcdd8926ed
A2kmoise <abayomoise950@gmail.com>
2026-03-08 08:27:54 +0200

    -{ * * }- AttachmentsRepository

---
+++ src/main/java/com/example/rib/Irepo/AttachmentsRepository.java
@@ -0,0 +1,7 @@
+package com.example.rib.Irepo;
+
+import com.example.rib.Imodel.Attachments;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttachmentsRepository extends JpaRepository<Attachments, String> {
+}
