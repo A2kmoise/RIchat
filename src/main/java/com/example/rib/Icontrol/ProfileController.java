74d14e4b12420a377814602b72b322f42d4860d9
A2kmoise <abayomoise950@gmail.com>
2026-03-27 20:40:46 +0200

    -{ * * }- profile controller

--- src/main/java/com/example/rib/Icontrol/ProfileController.java
+++ src/main/java/com/example/rib/Icontrol/ProfileController.java
@@ -1,4 +1,13 @@
 package com.example.rib.Icontrol;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ProfileController {
+
+    @PutMapping("/update-profile")
+    public void updateProfile(){}
+
+    @PutMapping("/update-password")
+    public void updatePassword(){}
 }
