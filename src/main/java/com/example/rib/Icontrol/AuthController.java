f4d49e6d3e8f143f7189dc1adf5b08f5ded75244
A2kmoise <abayomoise950@gmail.com>
2026-03-27 20:40:22 +0200

    -{ * * }- auth controller

--- src/main/java/com/example/rib/Icontrol/AuthController.java
+++ src/main/java/com/example/rib/Icontrol/AuthController.java
@@ -1,4 +1,17 @@
 package com.example.rib.Icontrol;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+
+    @PostMapping("/auth/register")
+    public void signUp(){}
+
+    @PostMapping("/auth/verify-otp")
+    public void verify(){}
+
+    @PostMapping("/auth/login")
+    public void login(){}
 }
