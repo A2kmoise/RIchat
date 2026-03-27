afefa5ab8cf69257863032209ed6d1a3ae4d6467
A2kmoise <abayomoise950@gmail.com>
2026-03-27 20:40:33 +0200

    -{ * * }- message controller

--- src/main/java/com/example/rib/Icontrol/MessageController.java
+++ src/main/java/com/example/rib/Icontrol/MessageController.java
@@ -1,4 +1,20 @@
 package com.example.rib.Icontrol;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class MessageController {
+
+    @PostMapping("/send-message")
+    public void sendMessage(){}
+
+    @GetMapping("/fetch-message")
+    public void fetchMessage(){}
+
+    @PutMapping("/update-message")
+    public void updateMessage(){}
+    @DeleteMapping("/delete-message")
+    public void deleteMessage(){}
+
+
 }
