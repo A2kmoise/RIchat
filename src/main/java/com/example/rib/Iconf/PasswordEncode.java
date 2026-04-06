0e58e9c37926d49f65308ed55c17e47361cf6d42
A2kmoise <abayomoise950@gmail.com>
2026-04-06 18:25:31 +0200

    -{ * * }- pass encoder fix

--- src/main/java/com/example/rib/Iconf/PasswordEncode.java
+++ src/main/java/com/example/rib/Iconf/PasswordEncode.java
@@ -2,9 +2,11 @@ package com.example.rib.Iconf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class PasswordEncoder {
+public class PasswordEncode {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
