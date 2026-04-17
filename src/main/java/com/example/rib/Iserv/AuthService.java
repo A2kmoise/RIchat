f1b43fe933e441b4baf2518f233e95bdcc5e2cfb
A2kmoise <abayomoise950@gmail.com>
2026-04-17 18:16:16 +0200

    -{ * * }- AuthService login and signup

--- src/main/java/com/example/rib/Iserv/AuthService.java
+++ src/main/java/com/example/rib/Iserv/AuthService.java
@@ -4,19 +4,25 @@ import com.example.rib.Idto.LoginRequest;
 import com.example.rib.Idto.RegisterRequest;
 import com.example.rib.Irepo.UsersRepository;
 import com.example.rib.Imodel.User;
+import com.example.rib.Iconf.TokenGenerator;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
 
     private final UsersRepository usersRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenGenerator tokenGenerator;
 
     public AuthService(UsersRepository usersRepository,
-                       PasswordEncoder passwordEncoder){
+                       PasswordEncoder passwordEncoder,
+                       TokenGenerator tokenGenerator){
         this.usersRepository = usersRepository;
         this.passwordEncoder = passwordEncoder;
+        this.tokenGenerator = tokenGenerator;
     }
 
     public String register(RegisterRequest registerRequest){
@@ -29,19 +35,23 @@ public class AuthService {
         // create new user
         User user = new User();
         user.setEmail(registerRequest.getEmail());
+        user.setUsername(registerRequest.getUsername());
         user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
 
         // save user
         usersRepository.save(user);
 
-        return "User registered successfully";
+        return tokenGenerator.tokenGenerate(user.getEmail());
     }
 
     public String login(LoginRequest loginRequest){
-        if(!usersRepository.existsUserByEmail(loginRequest.getEmail())){
-            throw new RuntimeException("User doesn't exist");
+        User user = Optional.ofNullable(usersRepository.findByEmail(loginRequest.getEmail()))
+                .orElseThrow(() -> new RuntimeException("User doesn't exist"));
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid password");
         }
-        // get token
-        return "Login successful";
+
+        return tokenGenerator.tokenGenerate(user.getEmail());
     }
 }
