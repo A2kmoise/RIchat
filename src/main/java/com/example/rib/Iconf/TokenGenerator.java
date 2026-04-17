1ec41adc521b631f0f676c52ded61777baefefa9
A2kmoise <abayomoise950@gmail.com>
2026-04-17 18:16:51 +0200

    -{ * * }-component

--- src/main/java/com/example/rib/Iconf/TokenGenerator.java
+++ src/main/java/com/example/rib/Iconf/TokenGenerator.java
@@ -7,6 +7,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 import java.util.function.Function;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TokenGenerator {
     private final  String SECRET_KEY = "SECRETfirst";
 
