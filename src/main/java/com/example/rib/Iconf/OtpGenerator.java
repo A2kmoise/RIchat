6c9b2c1b33753311f25b213b2bea9ea95ad8b09d
A2kmoise <abayomoise950@gmail.com>
2026-04-18 20:56:38 +0200

    -{ * * }- otp Generation

--- src/main/java/com/example/rib/Iconf/OtpGenerator.java
+++ src/main/java/com/example/rib/Iconf/OtpGenerator.java
@@ -1,11 +1,20 @@
 package com.example.rib.Iconf;
 
+import com.example.rib.Irepo.UsersRepository;
+import org.springframework.stereotype.Component;
+
 import java.security.SecureRandom;
 
+@Component
 public class OtpGenerator {
 private static final SecureRandom secure = new SecureRandom();
+    private final UsersRepository usersRepository;
 
-public String generateOtp(){
+    public OtpGenerator(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public String generateOtp(){
     int otp = 100000 + secure.nextInt(900000);
     return String.valueOf(otp);
 }
