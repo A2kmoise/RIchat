5ea5bf3dda51fd7e9f00a923365b1135f7855f9f
A2kmoise <abayomoise950@gmail.com>
2026-04-18 20:56:47 +0200

    -{ * * }- otp db access

---
+++ src/main/java/com/example/rib/Irepo/OtpRepository.java
@@ -0,0 +1,8 @@
+package com.example.rib.Irepo;
+
+import com.example.rib.Imodel.Otp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OtpRepository extends JpaRepository<Otp, String> {
+
+}
