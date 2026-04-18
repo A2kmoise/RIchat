70bae4b2e305c32a3f02ef34754c0e5ba716bc82
A2kmoise <abayomoise950@gmail.com>
2026-04-18 21:15:16 +0200

    -{ * * }- find otp

--- src/main/java/com/example/rib/Irepo/OtpRepository.java
+++ src/main/java/com/example/rib/Irepo/OtpRepository.java
@@ -3,6 +3,8 @@ package com.example.rib.Irepo;
 import com.example.rib.Imodel.Otp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OtpRepository extends JpaRepository<Otp, String> {
+import java.util.Optional;
 
+public interface OtpRepository extends JpaRepository<Otp, String> {
+    Optional<Otp> findTopByEmailOrderByCreatedAtDesc(String email);
 }
