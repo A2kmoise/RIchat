2d94a336def272edc26d8ade4717503f351da851
A2kmoise <abayomoise950@gmail.com>
2026-04-22 07:36:47 +0200

    -{ * * }- user Generics Optional<User>

--- src/main/java/com/example/rib/Irepo/UsersRepository.java
+++ src/main/java/com/example/rib/Irepo/UsersRepository.java
@@ -3,8 +3,10 @@ package com.example.rib.Irepo;
 import com.example.rib.Imodel.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<User, String> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
    boolean existsUserByEmail(String email);
 
 }
