179e4a9c969a11b7fce1740684a83d5463f57f1b
A2kmoise <abayomoise950@gmail.com>
2026-04-22 15:20:55 +0200

    -{ * * }- added jwtAuthFilter

--- src/main/java/com/example/rib/Iconf/SecurityConfig.java
+++ src/main/java/com/example/rib/Iconf/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -38,8 +39,8 @@ public class SecurityConfig {
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
-               // .addFilterBefore();
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 return http.build();
     }
 
