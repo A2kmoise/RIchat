8bacebb153b64c8db937e3b9fadbb54420d6976b
A2kmoise <abayomoise950@gmail.com>
2026-04-27 21:59:08 +0200

    -{ * * }- add: made fine dto

--- src/main/java/com/example/rib/Idto/MessageSendRequest.java
+++ src/main/java/com/example/rib/Idto/MessageSendRequest.java
@@ -1,9 +1,11 @@
 package com.example.rib.Idto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
+@NoArgsConstructor
 public class MessageSendRequest {
 
     @NonNull
@@ -14,4 +16,34 @@ public class MessageSendRequest {
 
     @NonNull
     private String conversationId;
+
+    public MessageSendRequest(@NonNull String content, @NonNull String id, @NonNull String conversationId) {
+        this.content = content;
+        this.id = id;
+        this.conversationId = conversationId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(String conversationId) {
+        this.conversationId = conversationId;
+    }
 }
