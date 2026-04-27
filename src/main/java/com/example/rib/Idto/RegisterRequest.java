5ff23c80ddaf498d70b04fcace6d53510f3f25ed
A2kmoise <abayomoise950@gmail.com>
2026-04-27 21:59:51 +0200

    -{ * * }- fix: made register request

--- src/main/java/com/example/rib/Idto/RegisterRequest.java
+++ src/main/java/com/example/rib/Idto/RegisterRequest.java
@@ -2,12 +2,14 @@ package com.example.rib.Idto;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.jspecify.annotations.NonNull;
 
 @Data
 @Getter
 @Setter
+@NoArgsConstructor
 public class RegisterRequest {
 
     private String email;
@@ -24,4 +26,27 @@ public class RegisterRequest {
         this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
