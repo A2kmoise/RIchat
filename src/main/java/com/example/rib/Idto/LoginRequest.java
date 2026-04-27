dcab51c4053abf5a71c960af2c2e6af659e47087
A2kmoise <abayomoise950@gmail.com>
2026-04-27 21:58:59 +0200

    -{ * * }- add: getter  setter

--- src/main/java/com/example/rib/Idto/LoginRequest.java
+++ src/main/java/com/example/rib/Idto/LoginRequest.java
@@ -1,10 +1,12 @@
 package com.example.rib.Idto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jspecify.annotations.NonNull;
 
 
 @Data
+@NoArgsConstructor
 public class LoginRequest {
 
     @NonNull
@@ -18,5 +20,19 @@ public class LoginRequest {
         this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
