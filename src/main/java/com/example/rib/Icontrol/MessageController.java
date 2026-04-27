dcce23ad6b723caf39b65095fe3a1876082470da
A2kmoise <abayomoise950@gmail.com>
2026-04-27 20:52:19 +0200

    -{ * * }- add: modified

--- src/main/java/com/example/rib/Icontrol/MessageController.java
+++ src/main/java/com/example/rib/Icontrol/MessageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/richat-api/v1/messages")
+@RequestMapping("/richat/api/v1/messages")
 public class MessageController {
 
     private final MessageService messageService;
