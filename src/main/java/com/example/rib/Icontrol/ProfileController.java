9e31acc740477ca26dc1c4e070b4deb15fa1a5df
A2kmoise <abayomoise950@gmail.com>
2026-04-27 20:36:27 +0200

    -{ * * }- add: put/profile and put/password profile endpoints

--- src/main/java/com/example/rib/Icontrol/ProfileController.java
+++ src/main/java/com/example/rib/Icontrol/ProfileController.java
@@ -1,14 +1,31 @@
 package com.example.rib.Icontrol;
 
+import com.example.rib.Iserv.ProfileService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/richat-api/v1/profile")
 public class ProfileController {
 
+    private final ProfileService profileService;
+
+    public ProfileController(ProfileService profileService){
+        this.profileService = profileService;
+    }
+
     @PutMapping("/profile")
-    public void updateProfile(){}
+    public void updateProfile(
+            @RequestParam String userId,
+            @RequestParam String newName
+    ){
+        profileService.updateProfile(userId, newName);
+    }
 
     @PutMapping("/password")
-    public void updatePassword(){}
+    public void updatePassword(
+            @RequestParam String userId,
+            @RequestParam String newPassword
+    ){
+        profileService.updatePassword(userId, newPassword);
+    }
 }
