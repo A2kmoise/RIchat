0a450fb2c7f84090911a0479b4dc03ec5e293e35
A2kmoise <abayomoise950@gmail.com>
2026-05-09 13:22:16 +0200

    -{ * * } swagger messages documentation

--- src/main/java/com/example/rib/Icontrol/MessageController.java
+++ src/main/java/com/example/rib/Icontrol/MessageController.java
@@ -3,10 +3,14 @@ package com.example.rib.Icontrol;
 import com.example.rib.Idto.MessageSendRequest;
 import com.example.rib.Imodel.Messages;
 import com.example.rib.Iserv.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Scanner;
 
+@Tag(name="messages")
 @RestController
 @RequestMapping("/richat/api/v1/messages")
 public class MessageController {
@@ -17,6 +21,7 @@ public class MessageController {
         this.messageService = messageService;
     }
 
+    @Operation(summary = "Sending messages", description ="Like creating a message to another user")
     @PostMapping("/message")
     public Messages sendMessage(
             @RequestBody MessageSendRequest sendRequest
@@ -28,6 +33,7 @@ public class MessageController {
         );
     }
 
+    @Operation(summary = "fetching messages", description = "fetching your related messages")
     @GetMapping("/message")
     public List<Messages> fetchMessage(
             @RequestParam String conversationId
@@ -35,6 +41,7 @@ public class MessageController {
         return messageService.fetchMessages(conversationId);
     }
 
+    @Operation(summary = "update message", description = "User update their messages")
     @PutMapping("/message")
     public Messages updateMessage(
             @RequestParam String userId,
@@ -43,6 +50,7 @@ public class MessageController {
     ){
         return messageService.updateMessages(userId, conversationId, newContent);
     }
+    @Operation(summary = "deleting messages", description = "delete messages by id")
     @DeleteMapping("/message")
     public String deleteMessage(
             @RequestParam String userId,
