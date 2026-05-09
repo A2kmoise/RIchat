e1ce98f284fd712eb09a9e349ccfbe47e98723fa
A2kmoise <abayomoise950@gmail.com>
2026-05-09 13:22:31 +0200

    -{ * * } swagger profile documentation

--- src/main/java/com/example/rib/Icontrol/ProfileController.java
+++ src/main/java/com/example/rib/Icontrol/ProfileController.java
@@ -1,8 +1,12 @@
 package com.example.rib.Icontrol;
 
 import com.example.rib.Iserv.ProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name="profile")
 @RestController
 @RequestMapping("/richat/api/v1/profile")
 public class ProfileController {
@@ -13,6 +17,7 @@ public class ProfileController {
         this.profileService = profileService;
     }
 
+    @Operation(summary = "updating profile", description = "user can update profile i.e username")
     @PutMapping("/profile")
     public void updateProfile(
             @RequestParam String userId,
@@ -21,6 +26,7 @@ public class ProfileController {
         profileService.updateProfile(userId, newName);
     }
 
+    @Operation(summary = "updating password", description = "user can update their password in case needed only password")
     @PutMapping("/password")
     public void updatePassword(
             @RequestParam String userId,
