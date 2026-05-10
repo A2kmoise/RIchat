382fe3616e3de7b1502739c042f443adc2c347dd
A2kmoise <abayomoise950@gmail.com>
2026-05-10 19:16:50 +0200

    -{ * * } register dto error handling

--- src/main/java/com/example/rib/Idto/RegisterRequest.java
+++ src/main/java/com/example/rib/Idto/RegisterRequest.java
@@ -1,5 +1,7 @@
 package com.example.rib.Idto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,41 +14,21 @@ import org.jspecify.annotations.NonNull;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "Email is required")
     private String email;
 
     @NonNull
+    @NotBlank(message = "Username is required")
     private String username;
 
     @NonNull
+    @NotBlank(message = "Password is required")
+    @Size(max = 6, message = "Password be at least 6 characters")
     private String password;
 
     public RegisterRequest(String email, @NonNull String username, @NonNull String password){
         this.email = email;
         this.username = username;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
