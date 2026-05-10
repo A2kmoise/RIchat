54f3dfa97928a7d2fb4a62722802b7bb4d82aa5b
A2kmoise <abayomoise950@gmail.com>
2026-05-10 19:15:56 +0200

    -{ * * } login dto error handling

--- src/main/java/com/example/rib/Idto/LoginRequest.java
+++ src/main/java/com/example/rib/Idto/LoginRequest.java
@@ -1,38 +1,31 @@
 package com.example.rib.Idto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.jspecify.annotations.NonNull;
 
 
 @Data
 @NoArgsConstructor
+@Getter
+@Setter
 public class LoginRequest {
 
     @NonNull
+    @NotBlank(message = "Email is required")
     private String email;
 
     @NonNull
+    @NotBlank(message = "Password is required")
+    @Size(max = 6, message = "password should be six characters")
     private String password;
 
     public LoginRequest(@NonNull String email, @NonNull String password){
         this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
