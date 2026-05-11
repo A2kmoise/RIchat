7cd3026692e413298d081db36ee12fc01f47b6d9
A2kmoise <abayomoise950@gmail.com>
2026-05-11 20:05:38 +0200

    -{ * * } Conversation repo

--- src/main/java/com/example/rib/Irepo/ConversationRepository.java
+++ src/main/java/com/example/rib/Irepo/ConversationRepository.java
@@ -1,7 +1,11 @@
 package com.example.rib.Irepo;
 
+import com.example.rib.Ienum.ConversationType;
 import com.example.rib.Imodel.Conversation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConversationRepository extends JpaRepository<Conversation, String> {
+    List<Conversation> conversationType(ConversationType conversationType);
 }
