8b6253d7146c7cd8f8e972ebda81ab4e7932d7ef
A2kmoise <abayomoise950@gmail.com>
2026-05-14 19:41:46 +0200

    -{ * * } message repo

--- src/main/java/com/example/rib/Irepo/MessagesRepository.java
+++ src/main/java/com/example/rib/Irepo/MessagesRepository.java
@@ -1,7 +1,6 @@
 package com.example.rib.Irepo;
 
 import com.example.rib.Imodel.Messages;
-import org.hibernate.boot.models.JpaAnnotations;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
